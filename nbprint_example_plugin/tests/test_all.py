7d2955d4029cf19d477f4ed44ab8d869f708c56e
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2026-08-09 09:57:41 -0400

    Refactor test_all.py by removing unused import
    Removed unnecessary import statement and cleaned up the test code.

--- nbprint_example_plugin/tests/test_all.py
+++ nbprint_example_plugin/tests/test_all.py
@@ -1,9 +1,5 @@
-<<<<<<< before updating
 class TestSearchpathPlugin:
     def test_discover_self(self):
         import hydra_plugins.lerna.searchpath
-=======
-from nbprint_example_plugin import *
->>>>>>> after updating
 
         assert "nbprint-example-plugin" in hydra_plugins.lerna.searchpath._searchpaths_pkg
